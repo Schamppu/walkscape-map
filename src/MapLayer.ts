62acb7c9c2d69a4f8e7c7a5b6c473e0ae954db9b
auttij <juhana.autti@gmail.com>
2024-03-12 19:52:08 +0200

    change marker culling to check map bounds instead of tiles

--- src/MapLayer.ts
+++ src/MapLayer.ts
@@ -1,20 +1,11 @@
-import {
-  LatLngBounds,
-  LayerGroup,
-  TileLayer,
-  Point,
-  LeafletEvent,
-} from "leaflet";
+import { LatLngBounds, LayerGroup, TileLayer } from "leaflet";
 import { WSMap } from "./WSMap";
-import { WSMarker } from "./WSMarker";
-import { MarkerContainer } from "./MarkerContainer";
 import { Layer, Visibility } from "./Layer";
 
 export class MapLayer extends LayerGroup {
   public tileLayer: TileLayer;
   public markerLayer: LayerGroup;
   private categories: Record<string, Layer[]> = {};
-  private tileMarkerContainers: MarkerContainer[][][] = [];
   private currentZoom = 1;
 
   public constructor(
@@ -32,31 +23,6 @@ export class MapLayer extends LayerGroup {
       noWrap: true,
     });
     this.markerLayer = new LayerGroup();
-
-    for (let z = 0; z <= map.getMaxZoom(); ++z) {
-      this.tileMarkerContainers[z] = [];
-      for (let x = 0; x < Math.pow(2, z + 2); ++x) {
-        this.tileMarkerContainers[z][x] = [];
-        for (let y = 0; y < Math.pow(2, z + 1); ++y) {
-          this.tileMarkerContainers[z][x][y] = new MarkerContainer();
-        }
-      }
-    }
-
-    this.tileLayer.on("tileload", (e: LeafletEvent) => {
-      const te = <L.TileEvent>e;
-      const container =
-        this.tileMarkerContainers[te.coords.z][te.coords.x][te.coords.y];
-      container.show();
-      container.getMarkers().forEach(this.updateMarkerVisibility.bind(this));
-    });
-    this.tileLayer.on("tileunload", (e: LeafletEvent) => {
-      const te = <L.TileEvent>e;
-      const container =
-        this.tileMarkerContainers[te.coords.z][te.coords.x][te.coords.y];
-      container.hide();
-      container.getMarkers().forEach(this.updateMarkerVisibility.bind(this));
-    });
   }
 
   public show(): void {
@@ -79,43 +45,38 @@ export class MapLayer extends LayerGroup {
 
   public addCategory(categoryName: string, layers: Layer[]): void {
     this.categories[categoryName] = layers;
+    const bounds = this.map.getBounds();
     layers.forEach((l) => {
       this.updateLayerVisibility(l);
-      l.markers.forEach((m) => {
-        this.addMarker(m, this.map.project(m.getLatLng(), 0));
-      });
+      l.updateMarkerVisibility(bounds);
     });
-  }
-
-  public addMarker(marker: WSMarker, point: Point) {
-    let isVisible = false;
-
-    // add to tile containers
-    for (let z = 0; z <= this.map.getMaxZoom(); ++z) {
-      const x = Math.floor((point.x * Math.pow(2, z)) / this.tileSize);
-      const y = Math.floor((point.y * Math.pow(2, z)) / this.tileSize);
-      this.tileMarkerContainers[z][x][y].addMarker(marker);
-
-      // marker is visible if tile container at ANY zoom level is visible
-      if (this.tileMarkerContainers[z][x][y].isVisible()) {
-        isVisible = true;
-      }
-
-      marker.addToTileContainer(this.tileMarkerContainers[z][x][y]);
-    }
-
-    if (isVisible) {
-      marker.show();
-    }
   }
 
   public updateZoom(zoom: number): void {
     this.currentZoom = zoom;
+    const bounds = this.map.getBounds().pad(0.2);
     for (const category of Object.values(this.categories)) {
       for (const layer of category) {
         this.updateLayerVisibility(layer);
+        this.updateLayerMarkerVisibility(layer, bounds);
       }
     }
+  }
+
+  public updateMarkersVisibility(): void {
+    const bounds = this.map.getBounds().pad(0.2);
+    for (const category of Object.values(this.categories)) {
+      for (const layer of category) {
+        this.updateLayerMarkerVisibility(layer, bounds);
+      }
+    }
+  }
+
+  private updateLayerMarkerVisibility(
+    layer: Layer,
+    bounds: LatLngBounds
+  ): void {
+    layer.updateMarkerVisibility(bounds);
   }
 
   private updateLayerVisibility(layer: Layer): void {
@@ -128,14 +89,6 @@ export class MapLayer extends LayerGroup {
       this.markerLayer.addLayer(layer);
     } else {
       this.markerLayer.removeLayer(layer);
-    }
-  }
-
-  private updateMarkerVisibility(marker: WSMarker): void {
-    if (marker.tileContainers.some((c) => c.isVisible())) {
-      marker.show();
-    } else {
-      marker.hide();
     }
   }
 }
