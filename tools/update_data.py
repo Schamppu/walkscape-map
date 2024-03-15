b448bd0f287bb174357c121782c5a3e812ac589a
Juhana Autti <ext-juhana.autti@3stepit.com>
2024-03-15 04:30:07 +0200

    Update data script

--- tools/update_data.py
+++ tools/update_data.py
@@ -10,6 +10,8 @@ def write_json(filepath, data):
         json.dump(data, jf, ensure_ascii=False, indent=2)
     
 def get_id(string):
+    if not string:
+        return None
     split = string.split('-')
     return '-'.join(split[1:len(split) - 5])
 
@@ -21,61 +23,58 @@ def get_name(official_location):
 def find(list, id):
     return [i for i in list if i['id'] == id]
 
-def update_locations():
-    locations_path = '../public/data/locations.json'
-    src_path = './locations_official.json'
+def read_data(data_path, src_path):
+    data = read_json(data_path)
+    src_data = read_json(src_path)
+    return data, src_data
 
-    location_json_full = read_json(locations_path)
-    locations = location_json_full[1]['layers'][0]['markers']
-    official_data = read_json(src_path)
+def get_common_info(official_obj, old_data, icon_key):
+    id = get_id(official_obj['id'])
+    old_obj = find(old_data, id)
+    exists = len(old_obj)
 
-    new_locations = []
+    # keep fixed capitalizations from old data if it exists
+    name = old_obj[0]['name'] if exists else get_name(official_obj)
+    icon_path = official_obj[icon_key].replace('assets/icons/', '')
+    return id, name, icon_path, old_obj
 
-    for off_location in official_data:
-        id = get_id(off_location['id'])
-        old_loc = find(locations, id)
-        exists = len(old_loc)
+def update_locations(filename):
+    data_path = f'../public/data/{filename}'
+    data_full, src_data = read_data(data_path, f'./{filename}')
+    data = data_full[1]['layers'][0]['markers']
 
-        # keep fixed capitalizations from old data if it exists
-        name = old_loc[0]['name'] if exists else get_name(off_location)
-        icon_path = off_location['locationIcon'].replace('assets/icons/', '')
+    locations = []
+    for src_location in src_data:
+        id, name, icon_path, old_loc = get_common_info(src_location, data, 'locationIcon')
         hidden = False if 'hidden' not in old_loc[0] else old_loc[0]['hidden']
 
         loc = {
             'id': id,
             'name': name,
-            'realm': off_location['realm'],
-            'coords': off_location['locationPosition'][::-1],
+            'realm': src_location['realm'],
+            'coords': src_location['locationPosition'][::-1],
             'icon': { 'url': icon_path },
             'hidden': hidden,
-            'activities': list(map(get_id, off_location['activityList'])),
-            'services': list(map(get_id, off_location['serviceList'])),
-            'buildings': list(map(get_id, off_location['buildingList'])),
+            'activities': list(map(get_id, src_location['activityList'])),
+            'services': list(map(get_id, src_location['serviceList'])),
+            'buildings': list(map(get_id, src_location['buildingList'])),
         }
-        new_locations.append(loc)
+        locations.append(loc)
 
-    location_json_full[1]['layers'][0]['markers'] = new_locations
-    write_json(locations_path, location_json_full)
+    data_full[1]['layers'][0]['markers'] = locations
+    write_json(data_path, data_full)
 
-def update_activities():
-    activities_path = '../public/data/activities.json'
-    src_path = './activities_official.json'
-    
-    activities_data = read_json(activities_path)
-    official_data = read_json(src_path)
+def update_activities(filename):
+    data_path = f'../public/data/{filename}'
+    data, src_data = read_data(data_path, f'./{filename}')
 
     activities = []
-    for off_activity in official_data:
-        id = get_id(off_activity['id'])
-        old_activity = find(activities_data, id)
-        exists = len(old_activity)
+    for src_activity in src_data:
+        id, name, icon_path, _ = get_common_info(src_activity, data, 'activityIcon')
 
-        # keep fixed capitalizations from old data if it exists
-        name = old_activity[0]['name'] if exists else get_name(off_activity)
-        icon_path = off_activity['activityIcon'].replace('assets/icons/', '')
-        level_requirements = off_activity['levelRequirementsMap']
+        level_requirements = src_activity['levelRequirementsMap']
         skills = [i for i in level_requirements.keys()]
-        required_keywords = [obj['keyword'] for obj in off_activity['requiredKeywords']]
+        required_keywords = [obj['keyword'] for obj in src_activity['requiredKeywords']]
 
         activity = {
             'id': id,
@@ -86,11 +85,31 @@ def update_activities():
             'requiredKeywords': required_keywords,
         }
         activities.append(activity)
-    write_json(activities_path, activities)
+    write_json(data_path, activities)
+
+def update_buildings(filename):
+    data_path = f'../public/data/{filename}'
+    data, src_data = read_data(data_path, f'./{filename}')
+    
+    buildings = []
+    for src_building in src_data:
+        id, name, icon_path, _ = get_common_info(src_building, data, 'iconPath')
+
+        building = {
+            'id': id,
+            'name': name,
+            'icon': icon_path,
+            'type': src_building['type'],
+            'shop': get_id(src_building['shop'])
+        }
+        buildings.append(building)
+        print(building)
+    write_json(data_path, buildings)
 
 def main():
-    #update_locations()
-    update_activities()
+    update_locations('locations.json')
+    update_activities('activities.json')
+    update_buildings('buildings.json')
 
 if __name__ == '__main__':
     main()
