867612c3257c97b388e3eb64c4db152d76ec16f3
Juhana Autti <ext-juhana.autti@3stepit.com>
2024-05-30 19:32:30 +0300

    Update CRS

--- src/WSCRS.ts
+++ src/WSCRS.ts
@@ -4,14 +4,14 @@ export function create(mapSize: number, tileSize: number): L.CRS {
   const scale = (2 * tileSize) / mapSize;
 
   // used map is wider than official for bigger tilesize
-  const baseWidthOffest = 1440;
-  const baseHeightOffest = 1488;
+  const baseWidthOffest = 180;
+  const baseHeightOffest = 185;
   const wscrs = Util.extend({}, CRS.Simple, {
     transformation: new Transformation(
       scale,
-      baseWidthOffest / 8,
+      baseWidthOffest,
       scale,
-      baseHeightOffest / 8
+      baseHeightOffest,
     ),
   });
 
